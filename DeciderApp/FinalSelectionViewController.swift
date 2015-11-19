109ce368d0203449e30a45205aa8f741b8ad7ab4
Miles Ranisavljevic <itstop3@gmail.com>
2015-11-19 15:17:56 -0800

    Set up FInalSelectionVC.  Adjust ParseService to take new finalSelection property into account.

--- DeciderApp/FinalSelectionViewController.swift
+++ DeciderApp/FinalSelectionViewController.swift
@@ -13,13 +13,36 @@ class FinalSelectionViewController: UIViewController {
     class func identifier() -> String {
         return "FinalSelectionViewController"
     }
+    
+    var event: Event?
+    
+    var venue: Venue?
+    
+    var eventID: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setUpView()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    func setUpView() {
+        guard let eventID = self.eventID else { return }
+        ParseService.loadEvent(eventID) { (success, event) -> () in
+            if success {
+                if let loadedEvent = event {
+                    if let loadedVenue = loadedEvent.finalSelection {
+                        self.event = loadedEvent
+                        self.venue = loadedVenue
+                    }
+                }
+            } else {
+                print("Error loading event.")
+            }
+        }
     }
 
 }
