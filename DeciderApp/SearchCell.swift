fe9ff877b8df69a6f5cc9ff8efb4f8c9973a017a
matthew weintrub <heytrub@gmail.com>
2015-11-19 09:02:57 -0800

    create search controller table view and search cell table view cell

---
+++ DeciderApp/SearchCell.swift
@@ -0,0 +1,61 @@
+//
+//  SearchCell.swift
+//  DeciderApp
+//
+//  Created by Matthew Weintrub on 11/18/15.
+//  Copyright © 2015 creeperspeak. All rights reserved.
+//
+
+import UIKit
+
+class SearchCell: UITableViewCell {
+    
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var ratingImageView: UIImageView!
+    @IBOutlet weak var reviewCountLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var checkboxButton: Checkbox!
+    
+    var venue: Venue! {
+        didSet {
+            nameLabel.text = venue.name
+            //            thumbnailImageView.image = venue.imageURL
+            categoryLabel.text = venue.categories
+            addressLabel.text = venue.address
+            reviewCountLabel.text = "\(venue.reviewCount!) Reviews"
+            //            ratingImageView.setImageWithURL(venue.ratingImageURL)
+//            distanceLabel.text = venue.distance
+            
+            
+        }
+    }
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        thumbnailImageView.layer.cornerRadius = 5
+        thumbnailImageView.clipsToBounds = true
+        
+        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
+        
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        nameLabel.preferredMaxLayoutWidth = nameLabel.frame.size.width
+        
+        
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
