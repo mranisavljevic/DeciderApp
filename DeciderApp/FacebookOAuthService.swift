0434521f703b680ba121b2e6692e2dd471b4bba2
Miles Ranisavljevic <itstop3@gmail.com>
2015-12-05 22:02:38 -0800

    Parse FB access token from url request.

--- DeciderApp/FacebookOAuthService.swift
+++ DeciderApp/FacebookOAuthService.swift
@@ -14,36 +14,26 @@ class FacebookOAuthService {
     let appId = "1116971171648123"
     let appSecret = "577640f16504fd444e46628132933b54"
     
+    var accessToken: String?
+    
     func requestAcessToken() {
         let baseURL = "https://graph.facebook.com/"
-        guard let requestURL = NSURL(string: "\(baseURL)oauth/access_token?client_id=\(appId)&client_secret=\(appSecret)&grant_type=client_credentials&redirect_uri=decider://") else { return }
-        print(requestURL)
+        guard let requestURL = NSURL(string: "\(baseURL)oauth/access_token?client_id=\(appId)&client_secret=\(appSecret)&grant_type=client_credentials") else { return }
         let request = NSMutableURLRequest(URL: requestURL)
         request.HTTPMethod = "GET"
-        request.setValue("application/json", forHTTPHeaderField: "Accept")
         NSURLSession.sharedSession().dataTaskWithRequest(request) { (data, response, error) -> Void in
             if let error = error {
                 print(error.localizedDescription)
             }
             if let data = data {
-                if let httpResponse = response as? NSHTTPURLResponse {
-                    print(httpResponse.statusCode)
-                    print(httpResponse.allHeaderFields)
-                    print(response)
-                }
                 self.parseAccessToken(data)
             }
         }.resume()
     }
     
     func parseAccessToken(data: NSData) -> String? {
-        print(NSJSONSerialization.isValidJSONObject(data))
-        do {
-            let jsonObject = try NSJSONSerialization.JSONObjectWithData(data, options: .MutableContainers)
-            if let rootObject = jsonObject as? [String : AnyObject] {
-                print(rootObject)
-            }
-        } catch { return nil }
+        guard let accessTokenString = String(data: data, encoding: NSUTF8StringEncoding) else { return nil }
+        self.accessToken = accessTokenString.stringByReplacingOccurrencesOfString("access_token=", withString: "")
         return nil
     }
 
